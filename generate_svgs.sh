df43faa24d689823d1e9897cb6ce4be7b321bc84
JonasVgt <git@jonasvoigt.de>
2025-09-01 13:30:54 +0200

    generate svgs for barrel, cairn and buoyant

---
+++ generate_svgs.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+declare -A color_values;
+
+color_values[amber]="#ffa000";
+color_values[black]="#000407";
+color_values[blue]="#0000ff";
+color_values[brown]="#a52a2a";
+color_values[green]="#00d400";
+color_values[grey]="#808080";
+color_values[magenta]="#f000f0";
+color_values[orange]="#ffa500";
+color_values[pink]="#ffc0cb";
+color_values[red]="#d40000";
+color_values[violet]="#ee82ee";
+color_values[white]="#ffffff";
+color_values[yellow]="#ffd400";
+
+names=('barrel' 'buoyant' 'cairn')
+
+for name in "${names[@]}"; do
+  while read -r pattern colorstring; do
+
+    colors=($colorstring)
+
+    if [[ "$pattern" == "solid" ]]; then
+      filename="svgs/$name/${colorstring// /_}.svg";
+      prefix="h";
+    else
+      filename="svgs/$name/$pattern/${colorstring// /_}.svg";
+      prefix=${pattern:0:1};
+    fi
+
+    echo "generating $filename";
+
+    
+    actions="";
+    for ((i=0; i<${#colors[@]}; i++)); do
+      fraction=$(( (i + 1) * 12 / ${#colors[@]} ))
+      id="path_fill_${prefix}${fraction}";
+      color_value=${color_values[${colors[i]}]};
+      actions="${actions}select-clear;select-by-id:$id;object-set-attribute:style,fill:${color_value};";
+    done
+
+    inkscape \
+      --actions="$actions" \
+      --export-plain-svg --export-filename="$filename" \
+      "svgs/$name/base.svg"
+  done < "svgs/$name/colors.txt"
+done
